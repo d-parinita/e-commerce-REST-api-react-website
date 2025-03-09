56ae9f9c16387dc92cc5a90f678709d62cd90304
Parinita Das <parinitad008@gmail.com>
2025-03-09 21:56:55 +0530

    hero sec update

--- src/app/Components/HeroSec/index.js
+++ src/app/Components/HeroSec/index.js
@@ -2,32 +2,25 @@ import React from 'react'
 import { IoIosReturnLeft } from "react-icons/io";
 import { HiOutlineBanknotes } from "react-icons/hi2";
 import { TbTruckDelivery } from "react-icons/tb";
+import Link from 'next/link';
+import { routes } from '@/app/utils/routes';
 
 export default function HeroSec() {
   return (
     <>
-          <div className='bg-[url(/image/bgImg.jpg)] bg-no-repeat bg-cover h-[60vh] relative opacity-50 bg-blend-multiply bg-gray-500 z-[-1] flex items-center justify-center'>
-              <div className='text-center'>
-                  <div className='text-4xl font-bold mb-4 text-white'>Lowest Prices Best Quality Shopping</div>
-                  <div className="flex flex-col sm:flex-row justify-center items-center sm:space-x-4 space-y-2 sm:space-y-0">
-                      <div className="flex items-center justify-center p-3 sm:p-[15px]">
-                          <TbTruckDelivery aria-hidden="true" className="size-5" />
-                          <span className="ml-2 text-sm sm:text-base">Free Delivery</span>
-                      </div>
+        <section className="relative w-full py-12 px-6 md:px-12 lg:px-24 flex flex-col-reverse lg:flex-row items-center justify-between">
+            <div className="lg:w-1/2 mb-4 text-center sm:pl-16 lg:text-left">
+                <h1 className="text-4xl md:text-5xl font-bold">Discover Your Style</h1>
+                <p className="mt-4 text-lg text-gray-400">Find the latest trends and best deals on our store. Shop now and redefine your wardrobe. Elevate your look with premium quality apparel and accessories, handpicked just for you.</p>
+                <div className="mt-6">
+                    <Link href={routes.ALLPRODUCTS} className="px-6 py-3 bg-lime-600 text-white text-lg font-medium rounded-lg shadow-md hover:bg-lime-700 transition-all">Shop Now</Link>
+                </div>
+            </div>
 
-                      <div className="flex items-center justify-center p-3 sm:p-[15px]">
-                          <HiOutlineBanknotes aria-hidden="true" className="size-5" />
-                          <span className="ml-2 text-sm sm:text-base">Cash on Delivery</span>
-                      </div>
-
-                      <div className="flex items-center justify-center p-3 sm:p-[15px]">
-                          <IoIosReturnLeft aria-hidden="true" className="size-5" />
-                          <span className="ml-2 text-sm sm:text-base">Easy Return</span>
-                      </div>
-                  </div>
-                  <div><button className='bg-lime-400 rounded px-[30px] py-[10px] text-gray-900'>Shop Now </button></div>
-              </div>
-          </div>  
+            <div className="lg:w-1/2 flex justify-center">
+                <img src="/image/bgImg2.jpg" alt="Hero Image" className="rounded-lg shadow-lg w-3/4 h-85 object-cover" />
+            </div>
+        </section>
     </>
   )
 }
